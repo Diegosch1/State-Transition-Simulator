d8f23adb55ecd00cf8f03470255968d18175633e
Diegoschi <diegoale.rodriguez15@gmail.com>
2025-09-06 16:20:40 -0500

    added basic controllers logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import SimulationController from './controllers/SimulationController';
-import { StateMachine } from './core/stateMachine';
 
-const stateMachine = new StateMachine();
+import React from "react";
+import StateDiagramComponent from "./components/state_diagram/StateDiagramComponent";
+
+
 
 function App() {
+  const processes = {
+    New: [{ pid: 1 }, { pid: 2 }],
+    Ready: [{ pid: 3 }],
+    Running: [{ pid: 4 }],
+    Waiting: [],
+    Terminated: [],
+  };
+
   return (
-    <div className="App">
-      <h1>Simulador de Estados de Procesos</h1>
-      <SimulationController stateMachine={stateMachine} />
+    <div>
+      <h1>Simulador de Estados</h1>
+      <StateDiagramComponent nodesData={processes} />
     </div>
-  );
-}
+  );}
 
 export default App;
